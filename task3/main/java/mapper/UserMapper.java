ae038eccedbb662ec33343aef87aaf897f29efae
annnstar <2587167885@qq.com>
2024-10-20 20:25:01 +0800

    Add files via upload

---
+++ task3/main/java/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.mybatis_example.mapper;
+
+import com.example.mybatis_example.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    public List<User> findAllUser();
+    public List<User> findUserByUserId(int userid);
+}
+
+
