c97774eec3370ed08927a6186c002433e94c56ba
annnstar <2587167885@qq.com>
2024-10-20 21:18:05 +0800

    Update UserMapper.java

--- task3/main/java/mapper/UserMapper.java
+++ task3/main/java/mapper/UserMapper.java
@@ -5,8 +5,10 @@ import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
+@Mapper //在运行时，会自动生成接口的实现类对象（代理对象），并且将该对象交给IOC的spring容器管理
 public interface UserMapper {
+    //查询全部用户信息
+    @Select("select * from user")//注解要执行的操作
     public List<User> findAllUser();
     public List<User> findUserByUserId(int userid);
 }
