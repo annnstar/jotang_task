4dea5d5960673e4823e98f53bbf2965f15b9765f
annnstar <2587167885@qq.com>
2024-10-20 21:20:03 +0800

    Update MybatisExampleApplicationTests.java

--- task3/test/MybatisExampleApplicationTests.java
+++ task3/test/MybatisExampleApplicationTests.java
@@ -3,10 +3,12 @@ package com.example.mybatis_example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest //springboot整合单元测试的注解
 class MybatisExampleApplicationTests {
-
-    @Test
+    //依赖注入
+    @Autowired
+    private UserMapper userMapper;
+    @Test //调用UserMapper中的接口信息
     void contextLoads() {
     }
 
