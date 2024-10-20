3cab01153068a2be094dc7da12092d77c8d371c7
annnstar <2587167885@qq.com>
2024-10-20 20:26:22 +0800

    Add files via upload

---
+++ task3/main/java/service/UserService.java
@@ -0,0 +1,25 @@
+package com.example.mybatis_example.service;
+
+
+import com.example.mybatis_example.entity.User;
+import com.example.mybatis_example.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired(required=false)
+    public UserMapper userMapper;
+
+    public List<User> findAllUser(){
+        return userMapper.findAllUser();
+    }
+
+    public List<User> findUserByUserId(int userid){
+        return userMapper.findUserByUserId(userid);
+    }
+}
+
+
